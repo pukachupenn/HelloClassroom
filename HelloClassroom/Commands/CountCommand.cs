57d4e94af660638907cbaf6a9216d23548657842
REDMOND\yoshita <yoshita@microsoft.com>
2017-02-09 13:58:50 -0800

    Partially implement CountCommand

--- HelloClassroom/Commands/CountCommand.cs
+++ HelloClassroom/Commands/CountCommand.cs
@@ -1,18 +1,43 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using HelloClassroom.Models;
+using Newtonsoft.Json;
 
 namespace HelloClassroom.Commands
 {
 	public class CountCommand : CommandBase
 	{
+		private int _fromCount;
+		private int _toCount;
+
+		private void ParseJson()
+		{
+			// TODO: Parse the LUIS payload here
+			//var request = JsonConvert.DeserializeObject<Dictionary<string, string>>(this.jsonData);
+
+			_fromCount = 0;
+			_toCount = 15;
+		}
+
 		public override Task ProcessCommand()
 		{
-			throw new System.NotImplementedException();
+			ParseJson();
+			return Task.FromResult(0);
 		}
 
 		public override DeviceCommand GenerateJsonPayload()
 		{
-			throw new System.NotImplementedException();
+			DeviceCommand command = new DeviceCommand()
+			{
+				Type = CommandType.Count,
+				Data = new Dictionary<string, string>
+				{
+					["from"] = _fromCount.ToString(),
+					["to"] = _toCount.ToString(),
+				}
+			};
+
+			return command;
 		}
 
 		public CountCommand(string jsonData) : base(jsonData)
