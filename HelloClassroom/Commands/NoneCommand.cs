ff3faf18694968a29ca91614a91d2e0629330a43
REDMOND\yoshita <yoshita@microsoft.com>
2017-02-10 13:54:22 -0800

    Add the None command

---
+++ HelloClassroom/Commands/NoneCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Fuzzy.Cortana;
+using HelloClassroom.Models;
+
+namespace HelloClassroom.Commands
+{
+	public class NoneCommand : CommandBase
+	{
+		public NoneCommand() : base(null)
+		{
+		}
+
+		public override Task ProcessCommand()
+		{
+			return Task.FromResult(0);
+		}
+
+		public override DeviceCommand GenerateJsonPayload()
+		{
+			DeviceCommand command = new DeviceCommand()
+			{
+				Type = CommandType.None
+			};
+
+			return command;
+		}
+	}
+}
