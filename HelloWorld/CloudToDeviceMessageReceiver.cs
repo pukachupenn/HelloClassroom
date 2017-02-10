ac654b996498bd84f9ebb0e7cfa977e8ab553eb5
Lifu Huang <lifu.hlf@gmail.com>
2017-02-10 11:14:28 -0800

    Add communication to client side.

---
+++ HelloWorld/CloudToDeviceMessageReceiver.cs
@@ -0,0 +1,34 @@
+﻿namespace HelloWorld
+{
+    using System.Threading.Tasks;
+    using System.Text;
+    using Microsoft.Azure.Devices.Client;
+
+    class CloudToDeviceMessageReceiver : IMessageReceiver
+    {
+        private DeviceClient DeviceClient { get; }
+
+        public CloudToDeviceMessageReceiver(string iotHubUri, string deviceId, string deviceKey, TransportType transportType = TransportType.Mqtt)
+        {
+            var credential = new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey);
+            DeviceClient = DeviceClient.Create(iotHubUri, credential, transportType);
+        }
+
+        public async Task<Message> ReceiveMessageAsync()
+        {
+            var receivedMessage = await DeviceClient.ReceiveAsync();
+            if (receivedMessage != null)
+            {
+                await DeviceClient.CompleteAsync(receivedMessage);
+            }
+
+            return receivedMessage;
+        }
+
+        public async Task<string> ReceiveStringMessageAsync()
+        {
+            var receivedMessage = await ReceiveMessageAsync();
+            return receivedMessage == null? null : Encoding.ASCII.GetString(receivedMessage.GetBytes());
+        }
+    }
+}
