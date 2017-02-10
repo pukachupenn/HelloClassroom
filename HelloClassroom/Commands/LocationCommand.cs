067d4437838b02ada2f5ac3349362ba2f6213f70
Ram Agrawal <ramagra@microsoft.com>
2017-02-09 16:57:04 -0800

    Refactor image helpers into util

--- HelloClassroom/Commands/LocationCommand.cs
+++ HelloClassroom/Commands/LocationCommand.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using HelloClassroom.Models;
+using HelloClassroom.Utils;
 using Newtonsoft.Json.Linq;
 
 namespace HelloClassroom.Commands
@@ -67,53 +68,9 @@ namespace HelloClassroom.Commands
 				}
 			}
 
-			locationData["Thumbnail"] = ConvertImageUrlToBase64((string) entityObject.SelectToken("image.thumbnailUrl"));
+			locationData["Thumbnail"] = ImageUtils.ConvertImageUrlToBase64((string) entityObject.SelectToken("image.thumbnailUrl"));
 
 			return locationData;
-		}
-
-		private static string ConvertImageUrlToBase64(string url)
-		{
-			StringBuilder stringBuilder = new StringBuilder();
-
-			byte[] _byte = GetImage(url);
-
-			stringBuilder.Append(Convert.ToBase64String(_byte, 0, _byte.Length));
-
-			return stringBuilder.ToString();
-		}
-
-		private static byte[] GetImage(string url)
-		{
-			byte[] buffer = null;
-
-			try
-			{
-				HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-
-				HttpWebResponse response = (HttpWebResponse)req.GetResponse();
-				Stream stream = response.GetResponseStream();
-
-				if(stream != null)
-				{
-					using (BinaryReader br = new BinaryReader(stream))
-					{
-						int len = (int)(response.ContentLength);
-						buffer = br.ReadBytes(len);
-						br.Close();
-					}
-
-					stream.Close();
-				}
-
-				response.Close();
-			}
-			catch (Exception)
-			{
-				buffer = null;
-			}
-
-			return buffer;
 		}
 
 		private static string BuildResponseUri(string location)
---
+++ HelloClassroom/Utils/ImageUtils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace HelloClassroom.Utils
+{
+	public static class ImageUtils
+	{
+		public static string ConvertImageUrlToBase64(string url)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+
+			byte[] _byte = GetImage(url);
+
+			stringBuilder.Append(Convert.ToBase64String(_byte, 0, _byte.Length));
+
+			return stringBuilder.ToString();
+		}
+
+		private static byte[] GetImage(string url)
+		{
+			byte[] buffer = null;
+
+			try
+			{
+				HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+				HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+				Stream stream = response.GetResponseStream();
+
+				if (stream != null)
+				{
+					using (BinaryReader br = new BinaryReader(stream))
+					{
+						int len = (int)(response.ContentLength);
+						buffer = br.ReadBytes(len);
+						br.Close();
+					}
+
+					stream.Close();
+				}
+
+				response.Close();
+			}
+			catch (Exception)
+			{
+				buffer = null;
+			}
+
+			return buffer;
+		}
+	}
+}
